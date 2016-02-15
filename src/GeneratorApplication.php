f6af185a515054b45fc97b3604328d7123dc00c0
Molodyko Ruslan <molodyko@samsonos.com>
2016-02-15 21:24:53 +0200

    Added submenu

--- src/GeneratorApplication.php
+++ src/GeneratorApplication.php
@@ -255,6 +255,28 @@ EOD;
 
         return $this->formatTab($this->generator->flush());
     }
+
+    public function createSubMenuView(Metadata $metadata, $namespace = __NAMESPACE__)
+    {
+
+$code = <<<'EOD'
+
+    <li>
+        <a class="sub_menu_a <?php if(isv('all_materials')):?>active<?php endif?>" href="<?php module_url()?>">
+            <i class="icon2 icon2-list"></i>
+            <?php t('All products')?>
+        </a>
+    </li>
+    <li>
+        <a class="sub_menu_a <?php if(isv('new_material')):?>active<?php endif?>" href="<?php module_url('new', 14);?>">
+            <i class="icon2 icon2-plus"></i> <?php t('Add product')?>
+        </a>
+    </li>
+
+EOD;
+
+        return str_replace('{{fields}}', implode(',', array_merge(array(), array("\n\t\t\t" . 'new Control()'. "\n\t\t"))), $code);
+    }
 }
 
 //[PHPCOMPRESSOR(remove,end)]
